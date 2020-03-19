cb9cb7235a0225405a48b0ff75ebee4aa8e49cad
Forestf <michalsliwa96@wp.pl>
2020-03-19 13:11:56 +0100

    Zad1

---
+++ WYK1_Levenshtein/src/com/levenshtein/Client.java
@@ -0,0 +1,37 @@
+package com.levenshtein;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        List<String> persons = new ArrayList<>();
+        String path ="names.txt";
+        String rootPerson = "Jan Kowalsky";
+
+        try {
+            persons = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int minimumDistance =Integer.MAX_VALUE;
+        int minimumLine =0;
+
+        for(String s: persons){
+            int currentDistance =Levenshtein.minimalDistance(rootPerson, s);
+            if(currentDistance<minimumDistance){
+                minimumDistance=currentDistance;
+                minimumLine=persons.indexOf(s);
+            }
+        }
+        // +1 przy założeniu że linie w pliku txt numerujemy od 1
+        System.out.println(minimumLine+1);
+    }
+}
---
+++ WYK1_Levenshtein/src/com/levenshtein/Levenshtein.java
@@ -0,0 +1,32 @@
+package com.levenshtein;
+
+public class Levenshtein {
+
+    public static int minimalDistance(String first, String second) {
+
+        int m = first.length()+1;
+        int n = second.length()+1;
+        int[][] distanceMatrix = new int[m][n];
+
+
+        for(int i=0; i<m; i++){
+           distanceMatrix[i][0] =i;
+        }
+        for(int j=1; j<n; j++){
+            distanceMatrix[0][j] =j;
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                int cost = 1;
+                if(first.charAt(i-1)==second.charAt(j-1)) cost =0;
+
+                distanceMatrix[i][j] = Math.min(distanceMatrix[i-1][j]+1,
+                        Math.min(distanceMatrix[i][j-1]+1, distanceMatrix[i-1][j-1]+cost));
+            }
+        }
+
+
+        return distanceMatrix[m-1][n-1];
+    }
+}
